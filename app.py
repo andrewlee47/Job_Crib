0bf710a41383ecd739c5cd6464112a62ec938261
Andrew <andrewbabatunde972gmail.com>
2023-08-26 16:16:19 +0100

    created a cloud database using aws rds and linking it with mysql workbench

--- app.py
+++ app.py
@@ -1,46 +1,23 @@
 from flask import Flask, render_template, jsonify
+from database import load_jobs_db
 
 app = Flask(__name__)
 app.static_folder = 'static'
 
-JOBS = [
 
-    {
-        "ID": 1,
-        "TITLE": "Virtual Assistance",
-        "LOCATION": "Texas",
-        "WAGES": "$30/hr"
-    },
 
-    {
-        "ID": 2,
-        "TITLE": "Data analyst",
-        "LOCATION": "Dallas",
-        "WAGES": "$40/hr"
-    },
-    {
-        "ID": 3,
-        "TITLE": "Book keeping",
-        "LOCATION": "California",
-        "WAGES": "$30/hr"
-    },
-    {
-        "ID": 4,
-        "TITLE": "Virtual Nurse",
-        "LOCATION": "Chicago",
-        "WAGES": "$50/hr"
-    }
-]
 
 
 @app.route("/")
-def hello_world():
-    return render_template('home.html', jobs=JOBS)
+def careerbuilder():
+    Job_list = load_jobs_db()
+    return render_template('home.html', jobs=Job_list)
 
 
 @app.route("/api/jobs")
 def list_jobs():
-    return jsonify(JOBS)
+    Job_list = load_jobs_db()
+    return jsonify(Job_list)
 
 
 
---
+++ database.py
@@ -0,0 +1,23 @@
+
+from sqlalchemy import create_engine, text
+import os
+
+
+database_url = os.environ.get('AWS_URL')
+
+engine = create_engine(database_url, pool_pre_ping=True)
+
+
+def load_jobs_db():
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT * FROM Jobs"))
+
+        Jobs = []
+        column_names = result.keys()  # Get column names from the result
+
+        for row in result:
+            job_dict = {column.lower(): value for column, value in zip(column_names, row)}
+            Jobs.append(job_dict)
+        return Jobs
+
+
