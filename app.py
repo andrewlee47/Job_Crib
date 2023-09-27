245dcb917a6f559bdc13a7247cd894031e84e107
Andrew <andrewbabatunde972gmail.com>
2023-09-27 16:53:33 +0100

    Made changes

--- app.py
+++ app.py
@@ -7,6 +7,15 @@ from database import load_jobs_db, job_details_db, text, engine
 app = Flask(__name__)
 
 
+#User uploaded files save directly to a folder on my pc
+app.static_folder = 'static'
+UPLOAD_FOLDER = "C:/Users/hp/Desktop/uploaded resumes files"
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 #homepage route
 @app.route("/")
 def careerbuilder():
@@ -23,17 +32,13 @@ def display_job(id):
         return "Not found", 404
     return render_template('jobpage.html', job = job)
 
-#User uploaded files save directly to a folder on my pc
-app.static_folder = 'static'
-UPLOAD_FOLDER = "C:/Users/hp/Desktop/uploaded resumes files"
-if not os.path.exists(UPLOAD_FOLDER):
-    os.makedirs(UPLOAD_FOLDER)
 
+@app.route("/job/<id>/apply", methods=['post'])
+def job_application(id):
+    data = request.form
+    job = job_details_db(id)
 
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-#Resume files save path
-def add_job(job_id, data):
+#Resume file path
     if 'Resume' in request.files:
         resume_files = request.files['Resume']
         if resume_files.filename != '':
@@ -41,39 +46,31 @@ def add_job(job_id, data):
             file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             resume_files.save(file_path)
 
-#saving every application to the database
-    with engine.connect() as conn:
-        query = text(
-            "INSERT INTO Applications (job_id,full_name, email, phone_number, experience, linkedIn, Resumes)"
-            " VALUES (:job_id, :full_name, :email, :phone_number, :experience, :linkedin, :resume_path)")
+#Saving every application to database
+    def add_job(job_id, data):
+        with engine.connect() as conn:
+            query = text(
+                        "INSERT INTO Applications (job_id,full_name, email, phone_number, experience, linkedIn, Resumes)"
+                        " VALUES (:job_id, :full_name, :email, :phone_number, :experience, :linkedin, :resume_path)")
 
-        conn.execute(query, {
-            'job_id': job_id,
-            'full_name': data['full_name'],
-            'email': data['email'],
-            'phone_number': data['phone_number'],
-            'experience': data['Experience'],
-            'linkedin': data['LinkedIn'],
-            'resume_path': file_path
-        })
+            conn.execute(query, {
+                        'job_id': job_id,
+                        'full_name': data['full_name'],
+                        'email': data['email'],
+                        'phone_number': data['phone_number'],
+                        'experience': data['Experience'],
+                        'linkedin': data['LinkedIn'],
+                        'resume_path': file_path
+                    })
 
-        conn.commit()
-
-
-@app.route("/job/<id>/apply", methods = ['POST'])
-def job_application(id):
-    data = request.form
-    job = job_details_db(id)
-    add_job(id, data)
-
-
-
+            conn.commit()
     return render_template('submitted_application.html', application = data, job = job)
 
 
 @app.route("/api/jobs")
 def list_jobs():
     Job_list = load_jobs_db()
+
     return jsonify(Job_list)
 
 
